e75fc4ed016d42cbdd15f54a78fccc2ff26217e6
Goutam Vishwakarma IIST <145545968+igoutamvIIST@users.noreply.github.com>
2024-11-28 12:38:43 +0530

    Update Question4.c

--- Question4.c
+++ Question4.c
@@ -18,3 +18,36 @@ Constraints:
 1 <= s.length <= 104
 s consists of parentheses only '()'.
 */
+
+#include <stdio.h>
+int main() {
+    char s[] = "(*))";
+    int open = 0;
+    int star = 0;
+    int n = sizeof(s) / sizeof(s[0]) - 1;
+
+    for (int i = 0; i < n; i++) {
+        if (s[i] == '(') {
+            open++;
+        } else if (s[i] == ')') {
+            if (open > 0) {
+                open--;
+            } else if (star > 0) {
+                star--;
+            } else {
+                printf("Output: false\n");
+                return 0;
+            }
+        } else if (s[i] == '*') {
+            star++;
+        }
+    }
+
+    if (open <= star) {
+        printf("Output: true\n");
+    } else {
+        printf("Output: false\n");
+    }
+
+    return 0;
+}
