dad62398db19e43d6390309faf3efc9127c6e123
Goutam Vishwakarma IIST <145545968+igoutamvIIST@users.noreply.github.com>
2024-11-28 12:27:27 +0530

    Update Question2.c

---
+++ Question2.c
@@ -0,0 +1,17 @@
+/*
+Given an array A of size N where, 1<= N <= 10^5. The task is to find the OR of all possible sub-arrays of A and then the OR of all these results.
+
+Examples:
+Input : 1 4 6
+Output : 7
+
+All possible subarrays are
+{1}, {1, 4}, {4, 6} and {1, 4, 6}
+ORs of these subarrays are 1, 5, 6
+and 7. OR of these ORs is 7.
+
+Input : 10 100 1000
+Output : 1006
+(Note: This question can also be asked in this way: after finding ORs of subarrays just return all
+distinct values of ORs)
+*/
