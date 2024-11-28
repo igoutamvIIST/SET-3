f0ce7974ed559a49b9d13c3e818cb998ec1496bd
Goutam Vishwakarma IIST <145545968+igoutamvIIST@users.noreply.github.com>
2024-11-28 12:29:47 +0530

    Create Question4.c

---
+++ Question4.c
@@ -0,0 +1,20 @@
+/*
+Given a string s containing only three types of characters: '(', ')' and '*', return true if s is valid.
+
+The following rules define a valid string:
+Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+Any right parenthesis ')' must have a corresponding left parenthesis '('.
+Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+
+Example 1:
+Input: s = "()"
+Output: true
+
+Example 2:
+Input: s = "(())"
+Output: true
+
+Constraints:
+1 <= s.length <= 104
+s consists of parentheses only '()'.
+*/
